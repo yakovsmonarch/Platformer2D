2139d298e7c33317666cb61213b6e8bb3953dd93
yakovsmonarch <medvejonok07@gmail.com>
2020-11-13 14:18:31 +0800

    Create repository

---
+++ MovementPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    [SerializeField] float _speed;
+    [SerializeField] float _jumpForce;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(_speed * -Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _jumpForce);
+        }
+    }
+}
